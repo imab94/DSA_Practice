744856ff3f41fbabe463c161a89b9b88e744553c
Arun Bhardwaj <arunbhardwaj@Aruns-MacBook-Pro.local>
2026-02-02 16:13:28 +0530

    program 1 sliding window

---
+++ DataStructurePatterns/Sliding_Window/Program1.py
@@ -0,0 +1,20 @@
+# Problem 1
+"""
+Maximum sum of subarray of size k
+Input:
+arr = [1,2,3,4,5,6] for k = 3
+Output: 15 (4+5+6)
+Constraints: 1<=n<=10^5
+"""
+
+ls = list(map(int,input().split()))
+k = 3
+window_sum = sum(ls[:k])
+max_sum = window_sum
+for right in range(k,len(ls)):
+    window_sum += ls[right]
+    window_sum -= ls[right-k]
+    max_sum = max(max_sum, window_sum)
+
+print(max_sum)
+
