bbdc247c894f893778d8cf67aff4b64339d2ad96
Arun Bhardwaj <arunbhardwaj@Aruns-MacBook-Pro.local>
2026-02-02 22:15:51 +0530

    Sliding Window: Solved longest substring without repeating

---
+++ DataStructurePatterns/Sliding_Window/Program4.py
@@ -0,0 +1,28 @@
+"""
+Sliding_Window.Program4
+Task
+Longest substring without repeating characters
+
+Input:
+s = "abcabcbb"
+
+Output: 3
+Constraints:
+1<=n<=10^5
+
+"""
+string = input()
+seen = set()
+left = 0
+max_len = 0
+
+for right in range(len(string)):
+    while string[right] in seen:
+        seen.remove(string[left])
+        left += 1
+    seen.add(string[right])
+    max_len = max(max_len, right-left+1)
+
+print(max_len)
+
+
