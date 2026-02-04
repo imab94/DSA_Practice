adffc5c65f181b30cd5378f5405843bdb8e42d11
Arun Bhardwaj <arunbhardwaj@Aruns-MacBook-Pro.local>
2026-02-04 23:36:50 +0530

    Solution: brute force, sliding window

--- DataStructurePatterns/Sliding_Window/Program2.py
+++ DataStructurePatterns/Sliding_Window/Program2.py
@@ -6,6 +6,19 @@ Output: 3
 Constraints: 1<=n<=10^5
 Hint: Use Sliding Window Fixed.
 """
+# brute force solution
+ls = list(map(int,input().split()))
+k = 2
+min_sum = float('inf')
+for i in range(len(ls)-k+1):
+    window_sum = 0
+    for j in range(i,i+k):
+        window_sum += ls[j]
+    min_sum  = min(window_sum, min_sum)
+
+print(min_sum)
+
+# Optimal solution using sliding window
 ls = list(map(int,input().split()))
 k = 2
 window_min = 0
