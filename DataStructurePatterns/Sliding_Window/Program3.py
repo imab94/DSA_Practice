3a20ccbfeb1d6b3f2799d9128cfeef7b099db786
Arun Bhardwaj <arunbhardwaj@Aruns-MacBook-Pro.local>
2026-02-04 23:43:33 +0530

    Solution: brute force, sliding window

--- DataStructurePatterns/Sliding_Window/Program3.py
+++ DataStructurePatterns/Sliding_Window/Program3.py
@@ -7,6 +7,21 @@ Output: [1.5,2.5,3.5]
 Constrainst: 1<=n<=10^5
 Hint: Use Sliding Window Fixed.
 """
+
+# brute force solution
+ls = list(map(int,input().split()))
+k = 2
+avg = []
+
+for i in range(len(ls)-k+1):
+    window_sum = 0
+    for j in range(i,i+k):
+        window_sum += ls[j]
+    avg.append(window_sum/2)
+
+print(avg)
+
+# Optimal solution using sliding window
 ls = list(map(int,input().split()))
 k = 2
 avg = []
